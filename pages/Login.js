c6813a65ad60e410facbb25cbd886228cb1f2d4f
Kuldeep Singh Bartwal <71578263+ksb44@users.noreply.github.com>
2024-07-08 09:20:12 +0530

    Update Login.js

--- pages/Login.js
+++ pages/Login.js
@@ -7,7 +7,7 @@ function Login() {
   let [credential, setCredential] = useState({ email: "", password: "" });
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch("http://localhost:3000/api/login", {
+    const response = await fetch("https://ecommerce-next-x3mg.onrender.com/api/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
